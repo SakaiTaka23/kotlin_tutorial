7bc92bd2cc43d2f79fd8c7ce7fb445f7101346a7
SakaiTaka23 <tt3.sakai@gmail.com>
2022-10-29 11:49:00 +0900

    secondary constructorについて学習

--- oop/src/main/kotlin/Main.kt
+++ oop/src/main/kotlin/Main.kt
@@ -1,17 +1,23 @@
 fun main(args: Array<String>) {
-    val user = User("lex", "Dobinica", 23)
-    println("Name: ${user.name}")
+    val user = User("Alex", "Dobinica", 23)
+    val user2 = User("Alex")
+    val user3 = User("Alex", "Smith")
 }
 
-class User(name: String, var lastName: String, var age: Int) {
-    var name: String
-
-    init {
-        if (name.lowercase().startsWith("a")){
-            this.name = name
-        } else {
-            this.name = "User"
-            println("The name doesn't start with the letter 'a' or 'A'")
-        }
+class User(var name: String, var lastName: String, var age: Int) {
+    constructor(name: String) : this(name,"LastName", 0) {
+        println("2nd")
     }
+    constructor(name: String, lastName: String): this(name, lastName, 0) {
+        println("3rd")
+    }
+
+//    init {
+//        if (name.lowercase().startsWith("a")){
+//            this.name = name
+//        } else {
+//            this.name = "User"
+//            println("The name doesn't start with the letter 'a' or 'A'")
+//        }
+//    }
 }
