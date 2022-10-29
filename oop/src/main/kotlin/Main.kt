f02a2eeab5cd5266e3071c1e33813ef5cc111ad0
SakaiTaka23 <tt3.sakai@gmail.com>
2022-10-29 15:43:06 +0900

    get, set周りについて学習

--- oop/src/main/kotlin/Main.kt
+++ oop/src/main/kotlin/Main.kt
@@ -1,23 +1,11 @@
 fun main(args: Array<String>) {
     val user = User("Alex", "Dobinica", 23)
-    val user2 = User("Alex")
+    val user2 = User(name = "Alex")
     val user3 = User("Alex", "Smith")
+
+    println(user.isAdult)
 }
 
-class User(var name: String, var lastName: String, var age: Int) {
-    constructor(name: String) : this(name,"LastName", 0) {
-        println("2nd")
-    }
-    constructor(name: String, lastName: String): this(name, lastName, 0) {
-        println("3rd")
-    }
-
-//    init {
-//        if (name.lowercase().startsWith("a")){
-//            this.name = name
-//        } else {
-//            this.name = "User"
-//            println("The name doesn't start with the letter 'a' or 'A'")
-//        }
-//    }
+class User(var name: String, var lastName: String = "LastName", var age: Int = 0) {
+    val isAdult get() = this.age > 20
 }
