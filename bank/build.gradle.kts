82217afd6c7462ce7d5f32088e80a2b0817b982f
SakaiTaka23 <tt3.sakai@gmail.com>
2022-12-16 18:04:36 +0900

    実際にテストを書いてみた

--- bank/build.gradle.kts
+++ bank/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.ninja-squad:springmockk:4.0.0")
 }
 
 tasks.withType<KotlinCompile> {
