3d384ce1ebd9b9f7d911ec07c918216c41f57a99
SakaiTaka23 <tt3.sakai@gmail.com>
2022-12-18 10:56:47 +0900

    設定を変えてみた

--- firstboot/README.md
+++ firstboot/README.md
@@ -18,4 +18,19 @@
 - それぞれのメソッド+Mappingでルートを作成していく
   - ルートを`/`にするとトレイリングスラッシュも必須になる
   - 必要ない場合は空白の文字列にする
-  - 
+
+## Configuration
+- 設定はresources/application.propertiesに配置
+  - 一覧 https://spring.pleiades.io/spring-boot/docs/current/reference/html/application-properties.html
+- 独自のプロパティ設定もできる
+  - プロパティを独自に記述
+  - `value`アノテーションを使って取得できる
+- 開発時と本番時でログレベルやDBの接続先などを変えたい時がある
+  - 環境に応じたプロパティを記述
+- プロパティファイルの名前は`application-環境.properties`とする
+  - そこに記述されていないがデフォルトのプロパティファイル(application.properties)にある場合はそれが適用される
+  - ある場合は上書きされる
+- プロパティファイルを有効にするにはビルドの実行構成を変更する
+- yamlを使って定義することもできる
+  - 一つのファイルに複数の構成を記述することができるようになる
+
--- firstboot/src/main/kotlin/com/example/firstboot/controller/HelloController.kt
+++ firstboot/src/main/kotlin/com/example/firstboot/controller/HelloController.kt
@@ -1,12 +1,20 @@
 package com.example.firstboot.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+data class Message(
+    val message: String,
+    val buildNumber: String
+)
+
 @RestController
-class HelloController {
+class HelloController(
+    @Value("\${example.firstboot.buildNumber}") val buildNumber: String
+) {
     @GetMapping("/hello")
-    fun hello(): String {
-        return "Hello"
+    fun hello(): Message {
+        return Message("Hello", buildNumber)
     }
 }
