fffdb29182ccec2931f200a802e044c31995d7d9
SakaiTaka23 <tt3.sakai@gmail.com>
2022-12-18 11:28:52 +0900

    ロガーを使ってログを出してみた

--- firstboot/README.md
+++ firstboot/README.md
@@ -34,3 +34,30 @@
 - yamlを使って定義することもできる
   - 一つのファイルに複数の構成を記述することができるようになる
 
+## Logging
+### 概要
+- 現状でログに使えるライブラリー
+1. Logback SpringBootのデフォルトロガー パフォーマンスを気にする時以外はこれで十分
+2. Log4j2 Logbackの問題を解決 パフォーマンスはより良い SpringBootで使うには多少設定が必要
+3. java.util.Logging SpringBootでは非推奨
+
+- Logging interfaceライブラリー
+1. SLF4J 基本これで問題ない
+2. Log4j2をそのまま使う 機能が増える代わりに他のロガーへの切り替えコストが高くなる
+3. Apache commons logging クラス周りで問題がある
+
+デフォルト構成はLogback * SLF4J
+
+### 使い方
+- loggerfactory.getloggerによりログのインスタンスを取得
+  - この時必要な引数はクラス名にしておく`this::class.java`
+- ログレベル
+  1. trace
+  2. debug
+  3. info
+  4. warn
+  5. error
+- デフォルトのログレベルはinfo
+  - 設定は`logging.level.root`で変えることができる
+  - このroot部は変更することによって自分のアプリケーションにのみと言ったこともできる
+- もちろんログに対して引数を与えることもできる
--- firstboot/src/main/kotlin/com/example/firstboot/controller/HelloController.kt
+++ firstboot/src/main/kotlin/com/example/firstboot/controller/HelloController.kt
@@ -1,5 +1,6 @@
 package com.example.firstboot.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,8 +14,17 @@ data class Message(
 class HelloController(
     @Value("\${example.firstboot.buildNumber}") val buildNumber: String
 ) {
+    val logger = LoggerFactory.getLogger(this::class.java)
+
     @GetMapping("/hello")
     fun hello(): Message {
+        logger.trace("trace")
+        logger.debug("debug")
+
+        logger.info("info")
+        logger.warn("warn")
+        logger.error("error")
+
         return Message("Hello", buildNumber)
     }
 }
