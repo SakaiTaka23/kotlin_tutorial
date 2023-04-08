c736366bc93bfdc42678ccc4b1cabbc4399a45e9
SakaiTaka23 <tt3.sakai@gmail.com>
2023-04-08 20:54:32 +0900

    認証をするために必要なサービスを定義

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
     implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1")
     implementation("mysql:mysql-connector-java:8.0.28")
     mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0")
+
+    implementation("org.springframework.boot:spring-boot-starter-security:2.7.10")
 }
 
 tasks.withType<KotlinCompile> {
